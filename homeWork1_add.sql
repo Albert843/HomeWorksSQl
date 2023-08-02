7df2c0eb19efb72612cdf9fe2f5f6240eb046d33
Albert843 <98774334+Albert843@users.noreply.github.com>
2023-08-02 16:32:32 +0300

    Create homeWork1_add.sql

---
+++ homeWork1_add.sql
@@ -0,0 +1,3 @@
+# SELECT * FROM mobile_phones WHERE product_name LIKE "%iPhone%";
+# SELECT * FROM mobile_phones WHERE manufacturer LIKE "%Samsung%";
+SELECT * FROM mobile_phones WHERE product_name LIKE "%8%";
